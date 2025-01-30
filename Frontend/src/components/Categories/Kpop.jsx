5793614b419709a0c082d35ec7885eabde954902
DEEKSHITHA M <153311203+DeekshithaMariyappan@users.noreply.github.com>
2025-01-30 20:52:04 +0530

    Update Kpop.jsx

--- Frontend/src/components/Categories/Kpop.jsx
+++ Frontend/src/components/Categories/Kpop.jsx
@@ -4,7 +4,7 @@ const AI = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3001/getKpopposts")
+    fetch("https://forumsystemboard.onrender.com/getAIposts")
       .then((res) => res.json())
       .then((data) => setPosts(data));
   }, []);
